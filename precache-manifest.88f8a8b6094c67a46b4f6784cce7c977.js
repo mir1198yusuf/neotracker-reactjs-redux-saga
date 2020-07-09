c9046f89fc85fd844ad71d2bdb453d3007dba163
mir1198yusuf <34382235+mir1198yusuf@users.noreply.github.com>
2020-07-10 01:36:40 +0530

    Updates

---
+++ precache-manifest.88f8a8b6094c67a46b4f6784cce7c977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c5d12786b1391e9e44934a30efe3af",
+    "url": "/neotracker-reactjs-redux-saga/index.html"
+  },
+  {
+    "revision": "d444f06df22b02331550",
+    "url": "/neotracker-reactjs-redux-saga/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9aec5fcbc187f40cee5a",
+    "url": "/neotracker-reactjs-redux-saga/static/css/main.f6d84dcc.chunk.css"
+  },
+  {
+    "revision": "d444f06df22b02331550",
+    "url": "/neotracker-reactjs-redux-saga/static/js/2.515cf6d7.chunk.js"
+  },
+  {
+    "revision": "9c5bdd2ce766e5bcd6a68258596993e3",
+    "url": "/neotracker-reactjs-redux-saga/static/js/2.515cf6d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aec5fcbc187f40cee5a",
+    "url": "/neotracker-reactjs-redux-saga/static/js/main.98567444.chunk.js"
+  },
+  {
+    "revision": "4e4a1c1218cef4283a5d",
+    "url": "/neotracker-reactjs-redux-saga/static/js/runtime-main.5e4c5a70.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/neotracker-reactjs-redux-saga/precache-manifest.88f8a8b6094c67a46b4f6784cce7c977.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/neotracker-reactjs-redux-saga/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
